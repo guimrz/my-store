e15911f1862f2e228e314b537e70df65c429e3bb
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-10 22:19:01 +0000

    Created catalog API and configured compose

--- src/Gateway/MyStore.Gateway/Program.cs
+++ src/Gateway/MyStore.Gateway/Program.cs
@@ -1,16 +1,21 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure services
-builder.Configuration.AddJsonFile("ocelot.json");
-builder.Services.AddOcelot().AddConsul();
+builder.Configuration
+    .AddJsonFile("ocelot.json")
+    .AddEnvironmentVariables();
 
+builder.Services.AddOcelot()
+    .AddConsul();
 
 var app = builder.Build();
 
-
+// Configure http request pipeline
+await app.UseOcelot();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
