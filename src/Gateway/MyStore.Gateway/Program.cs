e6e1f9020c872421d8da96ec89bc58e41a3cc065
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-10 18:25:35 +0000

    Added service discovery to ocelot

--- src/Gateway/MyStore.Gateway/Program.cs
+++ src/Gateway/MyStore.Gateway/Program.cs
@@ -1,11 +1,11 @@
-using Ocelot;
 using Ocelot.DependencyInjection;
+using Ocelot.Provider.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure services
 builder.Configuration.AddJsonFile("ocelot.json");
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddConsul();
 
 
 var app = builder.Build();
