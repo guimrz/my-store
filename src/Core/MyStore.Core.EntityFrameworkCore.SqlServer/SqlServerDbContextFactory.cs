f06add254d60f305fa98369c7fda1d8f67b07182
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-11 15:57:19 +0000

    Created libs for EF Core

---
+++ src/Core/MyStore.Core.EntityFrameworkCore.SqlServer/SqlServerDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace MyStore.Core.EntityFrameworkCore.SqlServer
+{
+    public class SqlServerDbContextFactory<TContext> : IDesignTimeDbContextFactory<TContext>
+        where TContext : DbContext
+    {
+        public TContext CreateDbContext(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json");
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
