48e9bca45b329dd7e8e1c753dd4fec299f65249c
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-11 15:26:46 +0000

    Revert "Added swagger to ocelot"
    This reverts commit f40009636b901b6617cc8406638c2ed651b81043.

--- src/Gateway/MyStore.Gateway/Program.cs
+++ src/Gateway/MyStore.Gateway/Program.cs
@@ -9,27 +9,13 @@ builder.Configuration
     .AddJsonFile("ocelot.json")
     .AddEnvironmentVariables();
 
-// Configure ocelot
 builder.Services.AddOcelot()
     .AddConsul();
-
-// Configure swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure http request pipeline
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseSwaggerForOcelotUI(opt =>
-{
-    opt.PathToSwaggerGenerator = "/swagger/docs";
-});
-
 await app.UseOcelot();
-
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
