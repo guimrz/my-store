8273baf85dc5036151d156904156b8fc70582332
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-11 13:50:27 +0000

    First commit

---
+++ src/Services/Catalog/MyStore.Services.Catalog.Domain/BrandAggregate/Brand.cs
@@ -0,0 +1,25 @@
+﻿#nullable disable
+namespace MyStore.Services.Catalog.Domain
+{
+    public class Brand
+    {
+        public Guid Id { get; protected set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreationDate { get; protected set; }
+
+        public DateTime? UpdateDate { get; protected set; }
+
+        protected Brand()
+        {
+            // Used by EF Core
+        }
+
+        public Brand(string name)
+        {
+            Name = name;
+            CreationDate = DateTime.UtcNow;
+        }
+    }
+}
