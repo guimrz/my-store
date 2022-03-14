e68dc113e430f864f83fb1f2977e843af2a80c48
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-14 19:15:44 +0000

    Added administration web app

---
+++ src/Web/Administration/MyStore.Web.Administration.App/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyStore.Web.Administration.App.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
