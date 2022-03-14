148c52ba5ceca2456a9e087c7d7909dcca76c7ea
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-14 13:46:35 +0000

    Created base identity project

---
+++ src/Services/Identity/MyStore.Services.Identity.API/Program.cs
@@ -0,0 +1,37 @@
+﻿using MyStore.Services.Identity.API;
+using MyStore.Services.Identity.Repository;
+using Serilog;
+using MyStore.Core.EntityFrameworkCore.Extensions;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();   
+
+    await app.MigrateDatabaseAsync<IdentityDbContext>();
+
+    app.Run();
+}
+catch (Exception ex) when (ex.GetType().Name is not "StopTheHostException") // https://github.com/dotnet/runtime/issues/60600
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
