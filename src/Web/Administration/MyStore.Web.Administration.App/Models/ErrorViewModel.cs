8479cf6b0c725653834f8a25b223fbf1f24984d4
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-14 19:15:44 +0000

    Added administration web app

---
+++ src/Web/Administration/MyStore.Web.Administration.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyStore.Web.Administration.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
