55eeb35e5c29dae59cc1413cbd4b0b2af5ada01f
José Guimarães <joseguimaraes96@hotmail.com>
2022-03-14 22:48:23 +0000

    Added category views to administration app

--- src/Web/Administration/MyStore.Web.Administration.App/Controllers/CategoriesController.cs
+++ src/Web/Administration/MyStore.Web.Administration.App/Controllers/CategoriesController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyStore.Web.Administration.App.Models.Categories;
+using MyStore.Web.Administration.App.Services;
 
 namespace MyStore.Web.Administration.App.Controllers
 {
     public class CategoriesController : Controller
     {
+        private readonly CatalogService _catalogService;
+		public CategoriesController(CatalogService catalogService)
+		{
+            _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
+		}
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> List(int count = 20, int offset = 0)
+		{
+            return View(await _catalogService.GetCategoriesAsync(count, offset));
+		}
+
+        public IActionResult Create()
+		{
+            return View(new CreateCategoryViewModel());
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryViewModel createCategoryViewModel)
+		{
+            await _catalogService.CreateCategoryAsync(createCategoryViewModel);
+
+            return RedirectToAction(nameof(List));
+		}
     }
 }
---
+++ src/Web/Administration/MyStore.Web.Administration.App/Services/CatalogService.cs
@@ -0,0 +1,50 @@
+﻿using MyStore.Web.Administration.App.Models.Categories;
+using Newtonsoft.Json;
+
+#nullable disable
+namespace MyStore.Web.Administration.App.Services
+{
+	public class CatalogService
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CatalogService(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+		}
+
+		public async Task<IEnumerable<CategoryViewModel>> GetCategoriesAsync(int count = 20, int offset = 0)
+		{
+			using var client = CreateClient();
+
+			var response = await client.GetAsync($"api/categories?count={count}&offset={offset}");
+
+			return await ReadResponseAsync<IEnumerable<CategoryViewModel>>(response);
+		}
+
+		public async Task<CategoryViewModel> CreateCategoryAsync(CreateCategoryViewModel category)
+		{
+			using var client = CreateClient();
+
+			var response = await client.PostAsync("api/categories", new StringContent(JsonConvert.SerializeObject(category), System.Text.Encoding.UTF8, "application/json"));
+
+			return await ReadResponseAsync<CategoryViewModel>(response);
+		}
+
+		private HttpClient CreateClient()
+		{
+			return _httpClientFactory.CreateClient("GatewayClient");
+		}
+
+		private static async Task<T> ReadResponseAsync<T>(HttpResponseMessage responseMessage)
+		{
+			responseMessage.EnsureSuccessStatusCode();
+
+			string content = await responseMessage.Content.ReadAsStringAsync();
+
+			T result = JsonConvert.DeserializeObject<T>(content);
+
+			return result;
+		}
+	}
+}
